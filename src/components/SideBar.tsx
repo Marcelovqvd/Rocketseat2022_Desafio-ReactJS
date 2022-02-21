ba329cde3b42e28d3ffcfc57969e20c1e27e70fd
Marcelo Vasques <marcelovqvd@gmail.com>
2022-02-21 15:18:40 -0300

    feat:Sidebar and Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+
+export function SideBar({selectedGenreId, handleClickButton, genres}: any) {
+  
+
+  return(
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres?.map((genre): any => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>     
+    </div>
+  )
 }
